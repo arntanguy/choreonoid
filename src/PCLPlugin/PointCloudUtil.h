91746a212508a7af45a7a9654b6e674070d3cf65
Arnaud TANGUY <arn.tanguy@gmail.com>
2015-04-30 13:25:58 +0900

    Add point to point ICP in Sim3 (estimate scale)

--- src/PCLPlugin/PointCloudUtil.h
+++ src/PCLPlugin/PointCloudUtil.h
@@ -15,6 +15,7 @@ namespace cnoid {
 CNOID_EXPORT SgMesh* createSurfaceMesh(SgPointSet* pointSet);
 CNOID_EXPORT boost::optional<double> alignPointCloud(SgPointSet* target, SgPointSet* source, Affine3& io_T, double maxCorrespondenceDistance, int maxIterations);
 CNOID_EXPORT boost::optional<double> alignPointCloudPointToPoint(SgPointSet* target, SgPointSet* source, Affine3& io_T, double maxCorrespondenceDistance, int maxIterations, double maxVariation);
+CNOID_EXPORT boost::optional<double> alignPointCloudPointToPointSim3(SgPointSet* target, SgPointSet* source, Affine3& io_T, double maxCorrespondenceDistance, int maxIterations, double maxVariation);
 
 }
 
